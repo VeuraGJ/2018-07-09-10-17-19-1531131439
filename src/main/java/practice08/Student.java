5fed228dc20e38edabfe2a36a67415bb341b6132
VeuraGJ <yuanxiao_ru@163.com>
2019-07-11 19:56:43 +0800

    feat:finished practice08 but person_with_same_id_be_same_one fail

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,30 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String isLeader = "Leader of";
+        if(klass.getLeader() == null){
+            isLeader = "at";
+        }else{
+            if(klass.getLeader().getName() != this.getName()){
+                isLeader = "at";
+            }
+        }
+        return String.format("%s I am a Student. I am %s %s.",super.introduce(), isLeader, klass.getDisplayName());
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,27 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduceWith(Student student){
+        return String.format("%s I am a Teacher. I %s %s.",super.introduce(),
+                student.getKlass().getNumber() == this.klass.getNumber() ? "teach" : "don't teach" ,
+                student.getName());
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Teacher. I teach %s.",super.introduce(),klass==null?"No Class":klass.getDisplayName());
+    }
 }
