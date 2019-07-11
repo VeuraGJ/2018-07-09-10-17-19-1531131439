d908d5cd6ad1a8538730a45438f87a73ca2e5191
VeuraGJ <yuanxiao_ru@163.com>
2019-07-11 19:14:42 +0800

    feat:finished practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String teachClass = this.klass > 0 ? "Class " + this.klass : "No Class";
+        return super.introduce() + " " + MessageFormat.format("I am a Teacher. I teach {0}.",teachClass);
+    }
 }
